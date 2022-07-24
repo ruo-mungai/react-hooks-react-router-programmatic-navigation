3467adbb5871c46cd0d5bbb46fdf5209b9c426ee
Patrick <ruomungai@gmail.com>
2022-07-24 10:34:25 +0300

    commit tree 0feef4cbf2169a23be569777e3b6c0861016237b

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,18 +15,25 @@ function Login({ setIsLoggedIn }) {
     });
   }
 
-  function handleSubmit(e) {
+    function handleSubmit(e) {
     e.preventDefault();
-
-    setIsLoggedIn(true);
-
-    // after logging the user in, redirect to the home page!
-    history.push("/");
+    fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((r) => r.json())
+      .then((user) => {
+        onLogin(user);
+        // after logging the user in, redirect to the home page!
+        history.push("/home");
+      });
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
       <input
         type="text"
         name="username"
@@ -43,5 +50,7 @@ function Login({ setIsLoggedIn }) {
     </form>
   );
 }
+
+  
 
 export default Login;
